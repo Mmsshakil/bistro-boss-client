c7c76d29409cc28aaebe0ac5f589f636be16e26f
Mmsshakil <softhandshakil@gmail.com>
2023-11-22 07:56:08 +0600

    delete from cart done

--- src/pages/Dashboard/Cart/Cart.jsx
+++ src/pages/Dashboard/Cart/Cart.jsx
@@ -1,9 +1,44 @@
+import Swal from "sweetalert2";
 import useCarts from "../../../hooks/useCarts";
+import { FaRegTrashAlt } from "react-icons/fa";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
 
 const Cart = () => {
-    const [cart] = useCarts();
+    const [cart, refetch] = useCarts();
     console.log(cart);
     const totalPrice = cart.reduce((total, item) => total + item.price, 0);
+    const axiosSecure = useAxiosSecure();
+
+    const handleDelete = id => {
+        console.log(id);
+
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                axiosSecure.delete(`/carts/${id}`)
+                    .then(res => {
+                        if (res.data.deletedCount > 0) {
+                            refetch();
+                            Swal.fire({
+                                title: "Deleted!",
+                                text: "Your file has been deleted.",
+                                icon: "success"
+                            });
+                        }
+                    })
+            }
+        });
+
+    }
+
+
     return (
         <div>
             <div className="flex justify-evenly items-center">
@@ -14,10 +49,11 @@ const Cart = () => {
 
             {/* table */}
             <div className="overflow-x-auto">
-                <table className="table">
+                <table className="table w-full">
                     {/* head */}
                     <thead>
                         <tr>
+                            <th>#</th>
                             <th>Image</th>
                             <th>Name</th>
                             <th>Price</th>
@@ -28,26 +64,27 @@ const Cart = () => {
 
 
                         {
-                            cart.map(item =>
+                            cart.map((item, index) =>
                                 <tr key={item._id}>
+                                    <td>{index + 1}</td>
                                     <td>
-                                        <div className="flex items-center gap-3">
-                                            <div className="avatar">
-                                                <div className="mask mask-squircle w-12 h-12">
-                                                    <img src={item.image} />
-                                                </div>
+                                        <div className="avatar">
+                                            <div className="mask mask-squircle w-12 h-12">
+                                                <img src={item.image} />
                                             </div>
-                                            
                                         </div>
                                     </td>
                                     <td>
-                                     {item.name}
+                                        {item.name}
                                     </td>
                                     <td>
-                                        {item.price}
+                                        ${item.price}
                                     </td>
                                     <th>
-                                        <button className="btn btn-ghost btn-xs">details</button>
+                                        <button className="btn btn-ghost">details</button>
+                                    </th>
+                                    <th>
+                                        <button onClick={() => handleDelete(item._id)} className="btn btn-ghost text-xl text-red-600"><FaRegTrashAlt></FaRegTrashAlt></button>
                                     </th>
                                 </tr>
                             )
